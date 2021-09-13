193f6e8ba6b4981def22d02d11c126ac95d76023
Mary Rankin <mary-rankin@outlook.com>
2021-09-13 12:30:13 -0400

    added some styling to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,43 @@
 import React from 'react';
 
 const Home = () => {
-    return <p>hi</p>
+    return <div className="container mt-5">
+        <div className="title-div mx-auto">
+            <h1 className="main-title d-flex justify-content-start">welcome to</h1>
+            <h1 className="main-title d-flex justify-content-end">music mates</h1>
+        </div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <div className="card mt-5">
+                        <img src="https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80" class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card mt-5">
+                        <img src="https://images.unsplash.com/photo-1511148672734-98014884c11e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80" class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card mt-5">
+                        <img src="https://images.unsplash.com/photo-1521931961826-fe48677230a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80" class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default Home;
